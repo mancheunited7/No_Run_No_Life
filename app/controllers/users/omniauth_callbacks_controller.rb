6b8f1fc575c77b8f40300102ca145a5c70d3d70f
森田啓太 <mancheunited7>
2018-03-19 10:22:08 +0900

    メッセージ表示

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -2,9 +2,9 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
   def sns_login
     @user = User.find_userinfo(request.env['omniauth.auth'])
-    
+
     if @user.persisted?
-      flash[:notice] = "ログインしました"
+      flash[:notice] = t('devise.sessions.signed_in')
       sign_in_and_redirect @user, event: :authentication
     else
       redirect_to new_user_session_path
