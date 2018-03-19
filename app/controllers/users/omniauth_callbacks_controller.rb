6c5e5e5e359e4b22f9ae84665aa2f70b359d6604
森田啓太 <mancheunited7>
2018-03-19 09:44:06 +0900

    snsログイン機能作成

---
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -0,0 +1,13 @@
+class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+
+  def sns_login
+    @user = User.find_userinfo(request.env['omniauth.auth'])
+    
+    if @user.persisted?
+      flash[:notice] = "ログインしました"
+      sign_in_and_redirect @user, event: :authentication
+    else
+      redirect_to new_user_session_path
+    end
+  end
+end
