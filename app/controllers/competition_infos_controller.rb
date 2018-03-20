6d17fd7c670ffb4c5760e8b374627f358021558e
森田啓太 <mancheunited7>
2018-03-21 00:38:11 +0900

    sprintf/変数名 修正

--- app/controllers/competition_infos_controller.rb
+++ app/controllers/competition_infos_controller.rb
@@ -8,23 +8,17 @@ class CompetitionInfosController < ApplicationController
 
   def create
     #ロードの大会情報取得
-    if params[:competition_info][:competition_classification] == '1'
+    if params[:competition_info][:competition_class] == '1'
       #1〜12月分の大会情報を取得
-      for i in 1..12 do
-        url = 'http://www.runnersbible.info/DB/DBM'
-        #1月〜9月と10月以降で取得するurlを区分
-        if i < 10
-          url = url + '0' + i.to_s + '.html'
-        else
-          url = url + i.to_s + '.html'
-        end
+      for month in 1..12 do
+        url = 'http://www.runnersbible.info/DB/DBM' + sprintf("%02i", month) + '.html'
         html = open(url) do |f|
           f.read
         end
         doc = Nokogiri::HTML.parse(html, nil, 'utf-8')
         #取得先のテーブル情報が２種類あるので区分分け
-        for j in 1..2 do
-          if j == 1
+        for classification in 1..2 do
+          if classification == 1
             competition_info = doc.css('tr.tblo')
           else
             competition_info = doc.css('tr.tble')
@@ -44,16 +38,17 @@ class CompetitionInfosController < ApplicationController
             str_competition_day = competition_day[0].to_s + '年' + competition_day[1].to_s + '月' +  competition_day[2].to_s + '日'
             @competition_info.competition_day = Date.strptime(str_competition_day,'%Y年%m月%d日')
             @competition_info.competition_place = info.css('td')[2].text
+            @competition_info.save
           end
         end
       end
     #トレイルの大会情報取得
     else
       now = Date.today
-      for k in 1..2 do
+      for classification in 1..2 do
         url = 'https://www.mtsn.jp/special/race_list'
         #本年分データ
-        if k == 1
+        if classification == 1
           str_year = now.year.to_s
           url = url + str_year + '.php'
         #翌年分データ
@@ -67,9 +62,9 @@ class CompetitionInfosController < ApplicationController
         doc = Nokogiri::HTML.parse(html, nil, 'utf-8')
         competition_info_table = doc.css('table.race_li_tbl')
         competition_info = competition_info_table.css('tr')
-        competition_info.each_with_index do |info, l|
+        competition_info.each_with_index do |info, index|
           #最初のループはカラムデータなのでとばす
-          if(l > 0)
+          if(index > 0)
             @competition_info = CompetitionInfo.new(competition_info_params)
             @competition_info.competition_name = info.at('a').text
             competition_day = info.at('td').text
@@ -88,5 +83,5 @@ end
 private
 
 def competition_info_params
-  params.require(:competition_info).permit(:competition_classification)
+  params.require(:competition_info).permit(:competition_class)
 end
