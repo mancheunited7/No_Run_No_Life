d8345f317ab5eeb517615fc4824d01dc096aacab
森田啓太 <mancheunited7>
2018-03-21 22:47:14 +0900

    アソシエーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable,
          :validatable, :confirmable, :omniauthable
+  has_many :run_records, dependent: :destroy
 end
