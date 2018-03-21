ea9361afd8d81258af186ea0cf329aed2775a856
森田啓太 <mancheunited7>
2018-03-21 23:48:46 +0900

    練習記録 レイアウト作成

---
+++ app/helpers/run_record/practices_helper.rb
@@ -0,0 +1,2 @@
+module RunRecord::PracticesHelper
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
   root 'top#index'
+  devise_for :users
   resources :mypages, only:[:index]
   resources :competition_infos, only:[:new, :create]
+  namespace :run_record do
+    resources :practices, only:[:new, :create]
+  end
 end
