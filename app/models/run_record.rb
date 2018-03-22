c7d1419f07892392ecd404c2e1105e7b3632994a
森田啓太 <mancheunited7>
2018-03-22 18:32:45 +0900

    練習記録登録機能作成

--- app/models/run_record.rb
+++ app/models/run_record.rb
@@ -1,6 +1,18 @@
 class RunRecord < ApplicationRecord
   belongs_to :user
-  has_one :weather_condition
-  has_one :competition_evaluation
-  has_one :body_state
+   has_one :weather_condition
+   has_one :competition_evaluation
+   has_one :body_state
+
+   accepts_nested_attributes_for :weather_condition, allow_destroy: true
+   accepts_nested_attributes_for :competition_evaluation, allow_destroy: true
+   accepts_nested_attributes_for :body_state, allow_destroy: true
+
+   def self.calc_time(hour, minute, second)
+     if hour.nil?
+       minute*60 + second
+     else
+       hour*3600 + minute*60 + second
+     end
+   end
 end
