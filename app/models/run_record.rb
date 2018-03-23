11a5dc473aa3a0b7de24b36d62273bd3128a6ea1
森田啓太 <mancheunited7>
2018-03-24 07:24:49 +0900

    ベストタイム表示

--- app/models/run_record.rb
+++ app/models/run_record.rb
@@ -18,4 +18,12 @@ class RunRecord < ApplicationRecord
      end
    end
 
+   def self.best_time_calc(comp_result)
+     best_time_list = {}
+     [5, 10, 21.0975, 42.195, 100].each do |distance|
+       best_time_list[distance] = comp_result.where(run_distance: distance).order(:run_calc_time).first
+     end
+     best_time_list
+   end
+
 end
