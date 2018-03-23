9dd48974a7ef245ea227e732cd34e8bade265a07
森田啓太 <mancheunited7>
2018-03-23 18:38:12 +0900

    アソシエーション設定

--- app/models/run_record.rb
+++ app/models/run_record.rb
@@ -3,10 +3,12 @@ class RunRecord < ApplicationRecord
    has_one :weather_condition, dependent: :destroy
    has_one :competition_evaluation, dependent: :destroy
    has_one :body_state, dependent: :destroy
+   has_one :competition_place, dependent: :destroy
 
    accepts_nested_attributes_for :weather_condition, allow_destroy: true
    accepts_nested_attributes_for :competition_evaluation, allow_destroy: true
    accepts_nested_attributes_for :body_state, allow_destroy: true
+   accepts_nested_attributes_for :competition_place, allow_destory: true
 
    def self.calc_time(hour, minute, second)
      if hour.nil?
